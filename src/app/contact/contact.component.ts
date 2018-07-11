8f2b8380df66f7dc459cd8f6a83bbc30e280c49b
DJ Rodriguez <djayr24@gmail.com>
2018-07-10 20:49:53 -0700

    worked on day3 contact app

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CONTACTS } from '../mock-contacts';
+import { Contact } from '../contact';
+
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
+  newContact: Contact = new Contact();
+  contacts = CONTACTS;
   constructor() { }
+
+  deleteContact(del){
+    const remove: number = this.contacts.indexOf(del);
+    if (remove !== -1) {
+      this.contacts.splice(remove, 1);
+    }
+  }
+
+  addContact() {
+    const lastIndex = this.contacts.length - 1;
+    const last = this.contacts[lastIndex];
+    this.newContact.id = last.id + 1;
+    this.contacts.push(this.newContact);
+    this.newContact = new Contact();
+  }
 
   ngOnInit() {
   }
