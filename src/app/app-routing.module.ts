69406ba4e67ce50566acaf180b1fbe1a7a22b54e
DJ Rodriguez <djayr24@gmail.com>
2018-07-12 19:49:04 -0700

    started todo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent} from './home/home.component';
 import { ContactComponent} from './contact/contact.component'
+import { TodoComponent } from './todo/todo.component'
+import { Todo } from './todo';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'contact', component: ContactComponent}
+  { path: 'contact', component: ContactComponent},
+  { path: 'todo', component: TodoComponent }
 ];
 
 @NgModule({
