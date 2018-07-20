1c0d91193ae3f2a75feb49e153a1f53943a86cf3
DJ Rodriguez <djayr24@gmail.com>
2018-07-19 20:10:15 -0700

    added forms for hero page with option to edit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,9 +9,16 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  hero: object;
+  hero: Hero;
   private temp: any;
+  newPower: string;
+  
   constructor(private route: ActivatedRoute) { }
+  
+  addPower() {
+    this.hero.powers.push(this.newPower);
+    this.newPower = '';
+  }
 
   ngOnInit() {
     this.temp = this.route.params.subscribe(params => {
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -2,5 +2,8 @@ export class Hero {
     id: number;
     name: string;
     hero: boolean;
-    
+    alter: string;
+    bio: string;
+    img: string;
+    powers: [string];
 }
