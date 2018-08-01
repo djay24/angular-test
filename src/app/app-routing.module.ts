828b02c00056c1311dc9293f083b59e37d0a92c8
DJ Rodriguez <djayr24@gmail.com>
2018-07-31 18:20:05 -0700

    added amiibo component and service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,14 @@ import { TodoComponent } from './todo/todo.component'
 import { HeroComponent } from './hero/hero.component';
 import { Hero } from './hero';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component'
-
+import { AmiibosComponent } from './amiibos/amiibos.component'
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactComponent},
   { path: 'todo', component: TodoComponent },
   { path: 'hero', component: HeroComponent },
-  { path: 'hero/:id', component: HeroDetailComponent }
+  { path: 'hero/:id', component: HeroDetailComponent },
+  { path: 'amiibos', component: AmiibosComponent }
 ];
 
 @NgModule({
