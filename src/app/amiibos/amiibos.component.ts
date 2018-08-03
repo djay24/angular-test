968f4a1a2cef76b89d8fb8055df6cbd959f8e007
DJ Rodriguez <djayr24@gmail.com>
2018-08-02 19:42:15 -0700

    pagination service shows only 12 items per page

--- src/app/amiibos/amiibos.component.ts
+++ src/app/amiibos/amiibos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AmiiboService } from '../services/amiibo.service';
 import { Amiibo } from '../interfaces/amiibo';
+import { PaginationService } from '../services/pagination.service'
 
 @Component({
   selector: 'app-amiibos',
@@ -9,12 +10,25 @@ import { Amiibo } from '../interfaces/amiibo';
 })
 export class AmiibosComponent implements OnInit {
   public amiibos: Amiibo[];
+  pager: any = {};
+  pagedItems: any = [];
 
-  constructor(private amiiboService: AmiiboService) { }
+  constructor(private amiiboService: AmiiboService, private paginationService: PaginationService ) { }
 
   getAmiibos(){
-    this.amiiboService.getAmiibos().subscribe((amiibos: any)=> this.amiibos = amiibos.amiibo);
+    this.amiiboService.getAmiibos().subscribe((amiibos: any) => {
+      this.amiibos = amiibos.amiibo;
+      this.setPage(1);
+    } );
   };
+  setPage(page: number) {
+    if (page < 1 || this.pager.totalPages) {
+      return;
+    }
+    this.pager = this.paginationService.getPager(this.amiibos.length, page);
+    this.pagedItems = this.amiibos.slice(this.pager.startIndex, this.pager.endIndex + 1);
+  }
+
 
   myFunction() {
     // this.amiiboService.myFunction();
--- src/app/services/pagination.service.ts
+++ src/app/services/pagination.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import * as _ from 'underscore';
+import { last } from '../../../node_modules/@angular/router/src/utils/collection';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,39 @@ import { Injectable } from '@angular/core';
 export class PaginationService {
 
   constructor() { }
-}
+  getPager(allItems: number, currentPage: number = 1, itemsOnPage: number = 12) {
+    const pagesCount = Math.ceil(allItems / itemsOnPage);
+    let firstPage: number, lastPage: number;
+
+    if (pagesCount <= 12) {
+      firstPage = 1;
+      lastPage = pagesCount;
+    } else {
+      if (currentPage <= 3) {
+        firstPage = 1;
+        lastPage = 12;
+      } else if(currentPage + 1 >= pagesCount) {
+        firstPage = pagesCount - 4;
+        lastPage = pagesCount;
+      } else {
+        firstPage = currentPage - 2;
+        lastPage = currentPage + 2;
+      }
+      }
+      const startIndex = (currentPage - 1 ) * itemsOnPage;
+      const endIndex = Math.min(startIndex + itemsOnPage - 1, allItems - 1);
+      const pages = _.range(firstPage, lastPage + 1);
+      return {
+        allItems: allItems,
+        currentPage: currentPage,
+        itemsOnPage: itemsOnPage,
+        pagesCount: pagesCount,
+        firstPage: firstPage,
+        lastPage: lastPage,
+        startIndex: startIndex,
+        endIndex: endIndex,
+        pages: pages
+      }
+    }
+  }
+
